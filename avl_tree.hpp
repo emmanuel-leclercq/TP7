b8f89eb2adf8f82a94be80f541e49fe5f4b47384
emmanuel-leclercq <68131409+emmanuel-leclercq@users.noreply.github.com>
2023-04-14 12:08:57 +0000

    init

--- avl_tree.hpp
+++ avl_tree.hpp
@@ -1,17 +1,26 @@
 #ifndef _AVL_TREE_HPP
 #define _AVL_TREE_HPP
 class Tree;
-class Node { private:
-long data; // étiquette du nœud
-int height; // hauteur du nœud dans un arbre Node * left;
-Node * right;
+class Node
+{
+private:
+    long data;  // étiquette du nœud
+    int height; // hauteur du nœud dans un arbre
+    Node *left;
+    Node *right;
+
 public:
-Node(long x); friend class Tree;
+    Node(long x);
+    Node() : height(1), left(nullptr), right(nullptr) {}
+    friend class Tree;
 };
-class Tree { private:
-            Node * root; // racine de l'arbre
+class Tree
+{
+private:
+    Node *root; // racine de l'arbre
 public:
-Tree(Node * r);
-Node * insert(long x); // plus tard : TreeIterator insert(long x); Node * search(long x); // plus tard : TreeIterator search(long x);
+    Tree(Node *r);
+    Tree() : root(nullptr) {}
+    Node *insert(long x); // plus tard : TreeIterator insert(long x); Node * search(long x); // plus tard : TreeIterator search(long x);
 };
 #endif
